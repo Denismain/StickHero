caa9579c73453332364b0d056bb45f65b12d1042
Denismain <bobigold12346@ya.ru>
2025-04-15 13:33:16 +0300

    Add Start Button

---
+++ assets/Scripts/PlayButton.ts
@@ -0,0 +1,24 @@
+import { GameManager } from './Managers/GameManager';
+import { GameState } from './GameStates/GameState';
+const { ccclass } = cc._decorator;
+
+@ccclass
+export default class PlayButton extends cc.Component {
+
+    onLoad() {
+        this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.node.on(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+    }
+
+    onTouchEnd() {
+        if (GameManager.Instance.gameState !== GameState.NONE) {
+            GameManager.Instance.gameState = GameState.NONE;
+            this.node.active = false;
+        }
+    }
+
+    onDestroy() {
+        this.node.off(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
+        this.node.off(cc.Node.EventType.TOUCH_CANCEL, this.onTouchEnd, this);
+    }
+}
